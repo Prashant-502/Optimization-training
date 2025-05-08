0624106e916aa30cb48f1181928fac4ff0effb78
Prashant-0207 <prashantshigvanofficial@gmail.com>
2025-05-08 17:13:03 +0530

    carousel commmented

--- js/main.js
+++ js/main.js
@@ -41,18 +41,18 @@
 
 
     // Header carousel
-    // $(".header-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     items: 1,
-    //     dots: true,
-    //     loop: true,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="bi bi-chevron-left"></i>',
-    //         '<i class="bi bi-chevron-right"></i>'
-    //     ]
-    // });
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
 
 
     // Testimonials carousel
